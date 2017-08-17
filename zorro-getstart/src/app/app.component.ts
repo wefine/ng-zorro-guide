d2dc69929e7a8467266810802835b2d4e9a51eed
wefine <wefine@aliyun.com>
2017-08-17 13:11:59 +0800

    zorro-getstart, add layout

--- zorro-getstart/src/app/app.component.ts
+++ zorro-getstart/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isCollapsed = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
